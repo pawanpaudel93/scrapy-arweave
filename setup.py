9a1f3ea83cee2fdecc85e5c786166536db5cbc27
Pawan Paudel <pawanpaudel93@gmail.com>
2023-05-16 19:10:15 +0545

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import io
 import os
+import platform
 import subprocess
 import sys
 from shutil import rmtree
@@ -7,12 +8,38 @@ from shutil import rmtree
 from setuptools import Command, setup
 from setuptools.command.install import install
 
+platform_commands = {
+    'Linux': {
+        'command': ['sudo', 'apt-get', 'install', '-y', 'libmagic1'],
+        'message': 'Failed to install libmagic1. Please install it manually with command: sudo apt-get install libmagic1',
+    },
+    'Windows': {
+        'command': ['pip', 'install', 'python-magic-bin'],
+        'message': 'Failed to install python-magic-bin. Please install it manually with command: pip install python-magic-bin',
+    },
+    'Darwin': {
+        'command': ['brew', 'install', 'libmagic'],
+        'message': 'Failed to install libmagic. Please install it manually with command: brew install libmagic',
+    },
+}
+
 
 class PostInstallCommand(install):
     def run(self):
         install.run(self)
-        # Execute post_install.py script
-        subprocess.run(['python', 'post_install.py'], check=True)
+
+        try:
+            current_system = platform.system()
+
+            if current_system in platform_commands:
+                command_info = platform_commands[current_system]
+                try:
+                    subprocess.run(command_info['command'], check=True)
+                except subprocess.CalledProcessError:
+                    print(command_info['message'])
+
+        except:
+            pass
 
 
 # Package meta-data.
